e84405505f2afbe9fa6f95de5d40817908486675
tungdd <tungdd98@gmail.com>
2024-08-17 16:53:00 +0700

    init project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,152 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import { FC, useMemo, useState } from 'react'
+import Image from 'next/image'
+import { cn } from '@/utils/helpers'
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+const IMAGES = {
+	BOY: '/images/boy.jpg',
+	GIRL: '/images/girl.jpg',
+} as const
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+const HomePage: FC = () => {
+	const [src, setSrc] = useState<string>(
+		localStorage.getItem('image') ?? IMAGES.BOY
+	)
+	const [name, setName] = useState('Chang e dun')
+	const [hour, setHour] = useState('10')
+	const [minute, setMinute] = useState('30')
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const { positionHour, positionMinute } = useMemo(() => {
+		if (src === IMAGES.BOY) {
+			return {
+				positionHour: 'top-[363px] right-[350px] w-full text-right',
+				positionMinute: 'top-[363px] right-[265px] w-full text-right',
+			}
+		}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+		return {
+			positionHour: 'top-[525px] left-[185px] w-1/2',
+			positionMinute: 'top-[525px] left-[250px] w-1/2',
+		}
+	}, [src])
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	const handleChangeSrc = (image: string) => {
+		setSrc(image)
+		localStorage.setItem('image', image)
+	}
+
+	return (
+		<div className='relative'>
+			<div className='flex justify-center items-center ml-[312px] min-h-screen'>
+				<div className='relative px-4'>
+					<Image
+						src={src}
+						width={1479 * 0.8}
+						height={1134 * 0.8}
+						alt=''
+						className='max-w-full'
+					/>
+					<div className='absolute z-10 top-[175px] left-0 w-1/2 text-lg text-center font-medium'>
+						{name}
+					</div>
+					<div
+						className={cn(
+							'absolute z-10 text-lg font-medium',
+							positionHour
+						)}>
+						{hour}
+					</div>
+					<div
+						className={cn(
+							'absolute z-10 text-lg font-medium',
+							positionMinute
+						)}>
+						{minute}
+					</div>
+				</div>
+			</div>
+			<div className='fixed bg-gray-100 shadow-md top-0 bottom-0 left-0 w-[312px] p-4'>
+				<button
+					type='button'
+					onClick={() => handleChangeSrc(IMAGES.BOY)}
+					className={cn(
+						'text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-4 w-full transition-all',
+						src === IMAGES.BOY && 'ring-4'
+					)}>
+					Thiệp nhà trai
+				</button>
+				<button
+					type='button'
+					onClick={() => handleChangeSrc(IMAGES.GIRL)}
+					className={cn(
+						'text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-10 w-full transition-all',
+						src === IMAGES.GIRL && 'ring-4'
+					)}>
+					Thiệp nhà gái
+				</button>
+				<form onSubmit={(e) => e.preventDefault()}>
+					<div className='mb-2'>
+						<label
+							htmlFor='first_name'
+							className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+							Tên khách mời
+						</label>
+						<div className='relative'>
+							<input
+								type='text'
+								id='first_name'
+								className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pr-7'
+								placeholder='Tên khách mời'
+								required
+								value={name}
+								onChange={(e) => setName(e.target.value)}
+							/>
+							{name && (
+								<div
+									className='absolute cursor-pointer right-3 top-1/2 -translate-y-1/2'
+									onClick={() => setName('')}>
+									&times;
+								</div>
+							)}
+						</div>
+					</div>
+					<div className='grid grid-cols-2 gap-2'>
+						<div>
+							<label
+								htmlFor='hour'
+								className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+								Giờ
+							</label>
+							<input
+								type='text'
+								id='hour'
+								className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+								required
+								value={hour}
+								onChange={(e) => setHour(e.target.value)}
+							/>
+						</div>
+						<div>
+							<label
+								htmlFor='minute'
+								className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+								Phút
+							</label>
+							<input
+								type='text'
+								id='hour'
+								className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+								required
+								value={minute}
+								onChange={(e) => setMinute(e.target.value)}
+							/>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	)
 }
+
+export default HomePage
